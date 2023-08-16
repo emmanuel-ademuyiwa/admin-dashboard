26b2b3d256f03ab7b69386962f2c8ac798ab0c30
Emmanuel Ademuyiwa <techienelson@gmail.com>
2023-08-16 11:23:09 +0100

    Initial and body design set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,5 @@
-import React from 'react';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  return <div className='app'></div>;
 }
 
 export default App;
