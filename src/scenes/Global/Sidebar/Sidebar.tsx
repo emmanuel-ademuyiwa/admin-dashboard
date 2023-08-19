60425f11597d3082d5efefcc4a99daac7931b2bf
Emmanuel Ademuyiwa <techienelson@gmail.com>
2023-08-19 18:09:05 +0100

    Modified Sidebar

--- src/scenes/Global/Sidebar/Sidebar.tsx
+++ src/scenes/Global/Sidebar/Sidebar.tsx
@@ -46,7 +46,6 @@ const SideBar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const [isCollapsed, setIsCollapsed] = useState(false);
-  const [selected, setSelected] = useState('Dashboard');
 
   return (
     <Box
